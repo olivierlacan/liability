aed6ac4806d27ec29bd8ff9831879ff117766bb2
Olivier Lacan <hi@olivierlacan.com>
2015-02-24 08:31:29 +0100

    Flesh out the recursive checking logic.

--- lib/liability/checker.rb
+++ lib/liability/checker.rb
@@ -1,3 +1,5 @@
+require "pathname"
+
 module Liability
   class Checker
     def self.invoke
@@ -6,14 +8,38 @@ module Liability
     end
 
     def self.check(args, err=$stderr, out=$stdout)
-      new.check
+      new.check(args)
     end
 
     def initialize
     end
 
-    def check
+    def check(args)
+      if args.include?('-r')
+        directory_option_index = args.index('-r') + 1
+        directory = args[directory_option_index]
+        recursive_check(directory)
+      else
+        simple_check(".")
+      end
+    end
 
+    def simple_check(directory)
+      puts "Doing a simple Ruby liability check on #{directory}..."
+    end
+
+    def recursive_check(directory)
+      sub_directories = Pathname.new(directory).children.select { |c| c.directory? }
+
+      puts "Doing a recursive Ruby liability check on #{directory}..."
+
+      if sub_directories.empty?
+        simple_check(directory)
+      else
+        sub_directories.each do |dir|
+          simple_check(dir)
+        end
+      end
     end
   end
 end
