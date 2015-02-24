e63af60233798c0255a0118cf1590c81428b188b
Olivier Lacan <hi@olivierlacan.com>
2015-02-24 21:23:21 +0100

    Implement full version checking logic.
    I've added a short list of MRI versions that are currently supported.
    Hardcoding this list is obviously not a great idea as I'd have to
    update the gem at irregular intervals, and I'd probably forget to do
    so when it matters most. I'll see if ruby-lang.org is interested in
    creating a small endpoint that returns currently supported versions.
    Same with JRuby, Rubinius, and other implementations.
    
    Something like ruby-lang.org/versions.json which would return:
    [
      "supported: [
        "2.0.0"
      ],
      "unsupported": [
        "1.8.7",
        "1.9.2",
        "1.9.3"
      ]
    ]
    
    The current output is not very nice when the current directory contains
    a lot of sub-directories so I'll have to see how to improve that. For
    directories with few sub-directories however it looks alright:
    
    $ liability -r  .
    Doing a recursive Ruby liability check on '.' ...
    Doing a simple Ruby liability check on 'elf' ...
    Warning: The following Ruby versions are used inside of 'elf':
    1.9.2

--- lib/liability/checker.rb
+++ lib/liability/checker.rb
@@ -2,9 +2,42 @@ require "pathname"
 
 module Liability
   class Checker
+    RUBY_VERSION_FILES = [ ".ruby-vesion", "Gemfile" ]
+    VERSION_NUMBER_PATTERN = /^(?:ruby)\s['"](\d.\d.\d)['"]$/
+    SUPPORTED_RUBY_VERSIONS = %w[
+      2.0.0-preview1
+      2.0.0-preview2
+      2.0.0-rc1
+      2.0.0-rc2
+      2.0.0-p0
+      2.0.0-p195
+      2.0.0-p247
+      2.0.0-p353
+      2.0.0-p451
+      2.0.0-p481
+      2.0.0-p576
+      2.0.0-p594
+      2.0.0-p598
+      2.1.0-dev
+      2.1.0-preview1
+      2.1.0-preview2
+      2.1.0-rc1
+      2.1.0
+      2.1.1
+      2.1.2
+      2.1.3
+      2.1.4
+      2.1.5
+      2.2.0-dev
+      2.2.0-preview1
+      2.2.0-preview2
+      2.2.0-rc1
+      2.2.0
+      2.3.0-dev
+    ]
+
     def self.invoke
-      status = check(ARGV, $stderr, $stdout).to_i
-      exit(status) if status != 0
+      check(ARGV, $stderr, $stdout)
     end
 
     def self.check(args, err=$stderr, out=$stdout)
@@ -24,14 +57,23 @@ module Liability
       end
     end
 
-    def simple_check(directory)
-      puts "Doing a simple Ruby liability check on #{directory}..."
+    def simple_check(pathname)
+      puts "Doing a simple Ruby liability check on '#{pathname}' ..."
+
+      files = Pathname.new(pathname).children.select { |c| c.file? }
+
+      if unsupported_ruby_versions?(files).empty?
+        puts "All clear! There are no unsupported Ruby versions used in '#{pathname}'."
+      else
+        puts "Warning: The following Ruby versions are used inside of '#{pathname}':"
+        puts unsupported_ruby_versions?(files)
+      end
     end
 
     def recursive_check(directory)
       sub_directories = Pathname.new(directory).children.select { |c| c.directory? }
 
-      puts "Doing a recursive Ruby liability check on #{directory}..."
+      puts "Doing a recursive Ruby liability check on '#{directory}' ..."
 
       if sub_directories.empty?
         simple_check(directory)
@@ -39,6 +81,40 @@ module Liability
         sub_directories.each do |dir|
           simple_check(dir)
         end
+      end
+    end
+
+    private
+
+    def find_referenced_ruby_versions(files)
+      ruby_version_files = find_ruby_version_files(files)
+      versions_detected = scan_ruby_version_files(ruby_version_files)
+    end
+
+    def find_ruby_version_files(files)
+      ruby_version_files = files.map(&:to_s).select do |file|
+        RUBY_VERSION_FILES.select { |version_file| file.include?(version_file) }
+      end
+    end
+
+    def scan_ruby_version_files(files)
+      files.map do |file|
+        versions = []
+
+        File.open file, "r" do |f|
+          versions = f.each_line.map do |line|
+            match = line.match(VERSION_NUMBER_PATTERN)
+            match ? match.captures.first : nil
+          end
+        end
+
+        versions.uniq
+      end
+    end
+
+    def unsupported_ruby_versions?(files)
+      find_referenced_ruby_versions(files).select do |version|
+        !SUPPORTED_RUBY_VERSIONS.include?(version)
       end
     end
   end
